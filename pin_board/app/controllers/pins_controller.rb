4ce18bdf889ba0e476f894494f54d55d8f7f736a
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2015-12-21 11:21:48 +0000

    Modified: creating new pin now working

--- pin_board/app/controllers/pins_controller.rb
+++ pin_board/app/controllers/pins_controller.rb
@@ -1,5 +1,11 @@
 class PinsController < ApplicationController
+    before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
 	def index
+	end
+
+	def show
+
 	end
 
 	def new
@@ -8,11 +14,20 @@ class PinsController < ApplicationController
 
 	def create
 		@pin = Pin.new(pin_params)
+		if @pin.save
+			redirect_to @pin, notice: 'Successfully created new Pin'
+		else
+			render 'new'
+		end
 	end
 
 	private
 
 	def pin_params
 		params.require(:pin).permit(:title, :description)
+	end
+
+	def find_pin
+		@pin = Pin.find(params[:id])
 	end
 end
