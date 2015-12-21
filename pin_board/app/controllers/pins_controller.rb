a709c2a5ae594a0c597efb003866e30b9aba0cee
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2015-12-21 11:37:24 +0000

    Modified: editing pin now working

--- pin_board/app/controllers/pins_controller.rb
+++ pin_board/app/controllers/pins_controller.rb
@@ -22,6 +22,20 @@ class PinsController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+	def update
+		if @pin.update(pin_params)
+			redirect_to @pin, notice: 'Pin was successfully updated'
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+	end
+
 	private
 
 	def pin_params
