7d9333797548e68d409e4fe973e5844282cf3b38
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2015-12-21 13:21:21 +0000

    Modified: added a demo user 'demo@pinboard.com' with password 'password' and associated the first pin with this user in the database using rails console.

--- pin_board/db/schema.rb
+++ pin_board/db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221130442) do
+ActiveRecord::Schema.define(version: 20151221131542) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
